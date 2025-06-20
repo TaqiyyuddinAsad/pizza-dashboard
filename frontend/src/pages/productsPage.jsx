79ae436cafa688d4fc50f4b8524f4c897d441197
Adam-Ibra <adam.ibra2504@gmail.com>
2025-06-20 19:51:31 +0200

    .

--- frontend/src/pages/productsPage.jsx
+++ frontend/src/pages/productsPage.jsx
@@ -2,33 +2,36 @@ import React from "react";
 import ProductBestsellerList from "../components/productsBestseller.jsx";
 import PopularCombinations from "../components/popularCombinations.jsx";
 import Layout from "../layout/layout";
+import ProductPerformanceChart from "../components/productPerformanceChart.jsx";
 
 const ProductPage = () => {
   return (
     <Layout>
-      <div className="p-6" style={{ maxWidth: "1400px", margin: "0 auto" }}>
-        <h2 className="text-2xl font-semibold mb-4">Waren – Analyse</h2>
+  <div className="p-6" style={{ maxWidth: "1400px", margin: "0 auto" }}>
+    <h2 className="text-2xl font-semibold mb-4">Waren – Analyse</h2>
 
-        <div
-          style={{
-            display: "flex",
-            gap: "24px",
-            alignItems: "flex-start",
-            flexWrap: "nowrap"
-          }}
-        >
-          {/* Linke Box: Bestseller */}
-          <div style={{ flex: 1, minWidth: "0", maxWidth: "850px" }}>
-            <ProductBestsellerList />
-          </div>
-
-          {/* Rechte Box: Kombinationen */}
-          <div style={{ width: "420px", flexShrink: 0 }}>
-            <PopularCombinations />
-          </div>
-        </div>
+    <div
+      style={{
+        display: "flex",
+        gap: "24px",
+        alignItems: "flex-start",
+        flexWrap: "nowrap"
+      }}
+    >
+      <div style={{ flex: 1, minWidth: "0", maxWidth: "850px" }}>
+        <ProductBestsellerList />
       </div>
-    </Layout>
+
+      <div style={{ width: "420px", flexShrink: 0 }}>
+        <PopularCombinations />
+      </div>
+    </div>
+
+    
+    <ProductPerformanceChart />
+  </div>
+</Layout>
+
   );
 };
 
